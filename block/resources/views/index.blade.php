268799f6904352423250bfb24659a39dbd2d395e
fermety <gregory501house@mail.ru>
2023-04-01 10:02:19 +0300

    migration: user,categories,posts,tags,post_tags

---
+++ block/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Юридическая консультация</title>    
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+</body> 
+</html>   
